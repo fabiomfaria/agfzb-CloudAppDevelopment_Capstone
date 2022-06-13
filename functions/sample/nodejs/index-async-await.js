29dfe0a34fddb00e62a3fe7baa54b7846e905cc4
Fábio de Faria <96897130+fabiomfaria@users.noreply.github.com>
2022-06-13 14:02:20 -0300

    updated version

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -2,22 +2,22 @@
  * Get all dealerships
  */
 
-const Cloudant = require('@cloudant/cloudant');
-const { IamAuthenticator } = require('ibm-cloud-sdk-core');
-
-async function main(params) {
-    const cloudant = Cloudant({
-        url: params.COUCH_URL,
-        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
-    });
+ const Cloudant = require('@cloudant/cloudant');
 
 
-    try {
-        let dbList = await cloudant.db.list();
-        return { "dbs": dbList };
-    } catch (error) {
-        return { error: error.description };
-    }
-
-}
+ async function main(params) {
+     const cloudant = Cloudant({
+         url: params.COUCH_URL,
+         plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+     });
+ 
+ 
+     try {
+         let dbList = await cloudant.db.list();
+         return { "dbs": dbList };
+     } catch (error) {
+         return { error: error.description };
+     }
+ 
+ }
 
