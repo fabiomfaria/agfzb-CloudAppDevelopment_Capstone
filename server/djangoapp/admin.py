7edd6d35d5a42e288096c95f0d9d5c0906b0b693
Fábio de Faria <96897130+fabiomfaria@users.noreply.github.com>
2022-06-13 14:36:06 -0300

    updated cardbs

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+# Register your models here.
+
+# CarModelInline class
 class CarModelInline(admin.StackedInline):
     model = CarModel
-
+    
+# CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ['name', 'dealerId', 'type', 'year']
 
+# CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
     list_display = ['name', 'description']
