b46c06ff9a5ae55f0c617590668e48bcd21fa183
fabiomfaria <fabiomfaria@gmail.com>
2022-06-14 18:23:28 +0000

    wat

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,9 +3,9 @@ import json
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
 
-from ibm_watson import NaturalLanguageUnderstandingV1, ApiException
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
+#from ibm_watson import NaturalLanguageUnderstandingV1, ApiException
+#from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+#from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
