8e7a330752bd0f8162f4129248a26074f8c5f248
fabiomfaria <fabiomfaria@gmail.com>
2022-06-14 19:48:22 +0000

    metodos

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,13 +10,12 @@ from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptio
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
-def get_request(url, api_key = None, **kwargs):
+def get_request(url, **kwargs):
     print(kwargs)
+    print("GET from {} ".format(url))
     try:
-        if api_key is not None and len(api_key) > 0:
-            response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs, auth=HTTPBasicAuth('api_key', api_key))
-        else:
-            response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
     except:
         print("Network exception occurred")
     status_code = response.status_code
@@ -27,9 +26,20 @@ def get_request(url, api_key = None, **kwargs):
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
 def post_request(url, json_payload, **kwargs):
-    print(kwargs)
     try:
-        response = requests.post(url, headers={'Content-Type': 'application/json'}, params=kwargs, json=json_payload)
+        response = requests.post(url, params=kwargs, json=json_payload)
+    except:
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
+
+
+def post_review(url, json_payload, **kwargs):
+    results = []
+    try:
+        response = requests.post(url, params=kwargs, json=json_payload)
     except:
         print("Network exception occurred")
     status_code = response.status_code
@@ -41,39 +51,47 @@ def post_request(url, json_payload, **kwargs):
 # def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
-def _get_dealers_from_cf_by_url(url):
+def get_dealers_from_cf(url, dealerId=-1):
     results = []
-    json_result = get_request(url)
+    if dealerId >= 0:
+        json_result = get_request(url,  dealerId=dealerId)
+    else:
+        json_result = get_request(url)
     if json_result:
-        dealers = json_result["body"]
-        for dealer in dealers:
-            # print(f"dealer={dealer}")
-            results.append(CarDealer.create(dealer))
+        if "rows" in json_result["body"] and len(json_result["body"]["rows"]) > 0:
+            dealers = json_result["body"]["rows"]
+            for dealer in dealers:
+                dealer_doc = dealer["doc"]
+                dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                    id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                    short_name=dealer_doc["short_name"],
+                                    st=dealer_doc["st"], zip=dealer_doc["zip"])
+                results.append(dealer_obj)
+        elif "docs" in json_result["body"] and len(json_result["body"]["docs"]) > 0:
+            dealer_doc = json_result["body"]["docs"][0]
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                short_name=dealer_doc["short_name"],
+                                st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
     return results
-
-
-def get_dealers_from_cf():
-    return _get_dealers_from_cf_by_url("https://5dbcd2a2.us-south.apigw.appdomain.cloud/dealership/get_dealerships")
-
-def get_dealer_by_id_from_cf(dealer_id):
-    return _get_dealers_from_cf_by_url(f"https://5dbcd2a2.us-south.apigw.appdomain.cloud/dealership/get_dealerships?dealer_id={dealer_id}")
-
-def get_dealers_by_state_from_cf(state):
-    return _get_dealers_from_cf_by_url(f"https://5dbcd2a2.us-south.apigw.appdomain.cloud/dealership/get_dealerships?state={state}")
   
   
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 # def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
-def get_dealer_reviews_from_cf(dealerId):
+def get_dealer_reviews_from_cf(url, dealerId):
     results = []
-    json_result = get_request(f"https://5dbcd2a2.us-south.apigw.appdomain.cloud/dealership/get_reviews/?dealerId={dealerId}")
-    if json_result and "body" in json_result:
-        reviews = json_result["body"]
+    json_result = get_request(url, dealerId=dealerId)
+    if json_result:
+        reviews = json_result["body"]["data"]["docs"]
         for review in reviews:
-            review['sentiment'] = analyze_review_sentiments(review['review'])
-            results.append(DealerReview.create(review))
+            review_obj = DealerReview(dealership=review["dealership"], name=review["name"], purchase=review["purchase"], review=review["review"], purchase_date=review["purchase_date"], car_make=review["car_make"], car_model=review["car_model"], car_year=review["car_year"], sentiment="positive", id=review["id"])
+            review_obj.sentiment = analyze_review_sentiments(review_obj.review)
+            results.append(review_obj)
+
     return results
 
 
